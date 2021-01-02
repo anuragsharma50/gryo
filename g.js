cee864476c1fb0dad31237f9cde4a3e15851b4ac
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2021-01-02 14:43:37 +0530

    Update g.js

--- g.js
+++ g.js
@@ -3,6 +3,7 @@ let gyroscope = new Gyroscope({frequency: 60});
 console.log('hello')
 
 gyroscope.addEventListener('reading', e => {
+  document.getElementById('#g').innerHTML = 'G - working';
   console.log("Angular velocity along the X-axis " + gyroscope.x);
   console.log("Angular velocity along the Y-axis " + gyroscope.y);
   console.log("Angular velocity along the Z-axis " + gyroscope.z);
@@ -10,17 +11,21 @@ gyroscope.addEventListener('reading', e => {
   document.getElementById('#y').innerHTML = gyroscope.y
   document.getElementById('#z').innerHTML = gyroscope.z
 });
+gryoscope.onerror = event => document.getElementById('#gerror').innerHTML = event.error.name, event.error.message;
 gyroscope.start();
 
 let acl = new Accelerometer({frequency: 60});
 
 acl.addEventListener('reading', () => {
+  document.getElementById('#a').innerHTML = 'A - working';
   console.log("Acceleration along the X-axis " + acl.x);
   console.log("Acceleration along the Y-axis " + acl.y);
   console.log("Acceleration along the Z-axis " + acl.z);
-  document.getElementById('#ax').innerHTML = gyroscope.x
-  document.getElementById('#ay').innerHTML = gyroscope.y
-  document.getElementById('#az').innerHTML = gyroscope.z
+  document.getElementById('#ax').innerHTML = acl.x
+  document.getElementById('#ay').innerHTML = acl.y
+  document.getElementById('#az').innerHTML = acl.z
 });
+
+acl.onerror = event => document.getElementById('#aerror').innerHTML = event.error.name, event.error.message;
 
 acl.start();
