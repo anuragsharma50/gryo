826a086eb664b860bdc5a2d2fe60a78c99116386
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2021-01-02 13:32:14 +0530

    Update g.js

--- g.js
+++ g.js
@@ -11,3 +11,13 @@ gyroscope.addEventListener('reading', e => {
   document.getElementById('#z').innerHTML = gyroscope.z
 });
 gyroscope.start();
+
+let acl = new Accelerometer({frequency: 60});
+
+acl.addEventListener('reading', () => {
+  console.log("Acceleration along the X-axis " + acl.x);
+  console.log("Acceleration along the Y-axis " + acl.y);
+  console.log("Acceleration along the Z-axis " + acl.z);
+});
+
+acl.start();
