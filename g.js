3f8e19b47037f87b9538346086097e7b9eefe86f
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2021-01-02 14:57:14 +0530

    Update g.js

--- g.js
+++ g.js
@@ -1,9 +1,7 @@
-let gyroscope = new Gyroscope({frequency: 10});
-
-console.log('hello')
+let gyroscope = new Gyroscope({frequency: 10})
 
 gyroscope.addEventListener('reading', e => {
-  document.getElementById('g').innerHTML = 'G - working';
+  document.getElementById('g').innerHTML = 'G - working updated';
   console.log("Angular velocity along the X-axis " + gyroscope.x);
   console.log("Angular velocity along the Y-axis " + gyroscope.y);
   console.log("Angular velocity along the Z-axis " + gyroscope.z);
